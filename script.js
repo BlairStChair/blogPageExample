cc0ca8b881ee3b21daec9c859053b7c7b2fee015
BlairStChair <julia.z15@interia.pl>
2024-08-15 14:09:55 +0200

    Wrote event listener for button to submit email for newsletter

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const takeMeToQuizButton = document.querySelector("#takeMeToQuiz");
 takeMeToQuizButton.addEventListener("click", () =>{
     location.href = 'html/quiz.html';
 });
+
+const emailInput = document.querySelector("#emailInput");
+let emailValue = emailInput.value;
+const done = document.querySelector("#done");
+const newsletterText = document.querySelector("#newsletterText");
+const newsletterForm = document.querySelector("#newsletterForm");
+
+done.addEventListener("click", () =>{
+    if (emailValue === '') {
+        alert("You can't leave the field empty!");
+    } else {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailRegex.test(emailValue) === true) {
+            alert("Nice");
+        } else {
+            alert("Wrong");
+        }
+    }
+});
+
