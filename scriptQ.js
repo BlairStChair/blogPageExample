9e7898dee5b27c6bfc2db0dcbc5e4c5ccda7aa82
BlairStChair <julia.z15@interia.pl>
2024-08-29 17:45:05 +0200

    Added function to get to the second question of the quiz

--- scriptQ.js
+++ scriptQ.js
@@ -4,10 +4,14 @@ const quizHeader = document.querySelector("#quizHeader");
 const quizDesc = document.querySelector("#quizDesc");
 const quizBtnContainer = document.querySelector("#quizBtnContainer");
 
-let questionsArray = ["What type of location do you have available in your home?"];
-let answerArray = ["Sunny; Sunny, but with diffused ligth; Shady"];
+let questionsArray = ["What type of location do you have available in your home?", "Do you prefer an easy-to-grow plant or something more challenging?"];
+let answerArray = ["Sunny; Sunny, but with diffused ligth; Shady", "Easy to grow; Challenging"];
 
 let readyAnswers = [];
+
+let answersCount = [];
+
+let questionNumber = -1;
 
 //function to separate question's answers
 function separateAnswer(n){
@@ -17,13 +21,21 @@ function separateAnswer(n){
 
 const startQuiz = document.querySelector("#startQuiz");
 
+let nextQuestionBtn = document.createElement("button");
+    nextQuestionBtn.id = "startQuiz";
+    nextQuestionBtn.textContent = "Next Question";
+
+let question = document.createElement("h2");
+
 startQuiz.addEventListener("click", () => {
+    questionNumber++;
+
     quizHeader.remove();
     quizDesc.remove();
     startQuiz.remove();
 
-    let question = document.createElement("h2");
-    question.textContent = questionsArray[0];
+    
+    question.textContent = questionsArray[questionNumber];
     questionName.appendChild(question);
 
     let answersList = document.createElement("ul");
@@ -38,14 +50,9 @@ startQuiz.addEventListener("click", () => {
         divForAnswersDivs.appendChild(answersList);
 
     for(let i = 0; i < firstQuestionAnswers.length; i++){
-
-        
-
         let answersDivs = document.createElement("div");
         answersDivs.className = "answersDivs";
         answersList.appendChild(answersDivs);
-
-        
 
         let label = document.createElement("label");
         label.textContent = readyAnswers[i].trim();
@@ -56,8 +63,43 @@ startQuiz.addEventListener("click", () => {
         answersDivs.appendChild(br);
     }
 
-    let nextQuestionBtn = document.createElement("button");
-    nextQuestionBtn.id = "startQuiz";
-    nextQuestionBtn.textContent = "Next Question";
+    
     quizBtnContainer.appendChild(nextQuestionBtn);
+
+    
+});
+
+nextQuestionBtn.addEventListener("click", () =>{
+    questionNumber++;
+
+    question.textContent = questionsArray[questionNumber];
+    questionName.appendChild(question);
+
+    while (questionAnswers.firstChild) {
+        questionAnswers.removeChild(questionAnswers.firstChild);
+    }
+
+    let currentQuestionAnswers = separateAnswer(questionNumber);
+
+    let answersList = document.createElement("ul");
+    questionAnswers.appendChild(answersList);
+
+    let divForAnswersDivs = document.createElement("div");
+    divForAnswersDivs.id = "divForAnswersDivs";
+    questionAnswers.appendChild(divForAnswersDivs);
+    divForAnswersDivs.appendChild(answersList);
+
+    for (let i = 0; i < currentQuestionAnswers.length; i++) {
+        let answersDivs = document.createElement("div");
+        answersDivs.className = "answersDivs";
+        answersList.appendChild(answersDivs);
+
+        let label = document.createElement("label");
+        label.textContent = currentQuestionAnswers[i].trim();
+    
+        answersDivs.appendChild(label);
+    
+        let br = document.createElement("br"); 
+        answersDivs.appendChild(br);
+    }
 });
