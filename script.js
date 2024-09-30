ce71b7f796397923b497070d16de817c98666d5a
BlairStChair <julia.z15@interia.pl>
2024-09-30 19:22:54 +0200

    Added dark mode for homepage

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const darkMode = document.querySelector("#darkMode");
+const links = document.querySelectorAll("a");
+const contentPhoto = document.querySelector("#contentPhoto");
+const content = document.querySelector("#content");
+const quizText = document.querySelector("#quizText");
+const takeMeToQuiz = document.querySelector("#takeMeToQuiz");
+const newsletterPanel = document.querySelector("#newsletterPanel");
+
+let clickCounter = 0;
+
+darkMode.addEventListener("click", () => {
+    clickCounter++;
+
+    if (clickCounter % 2 == 1) {
+        document.body.style.backgroundColor = "#1A1A1A";
+        links.forEach(links => {
+            links.style.color = "#f3f3f2"; 
+        });
+        contentPhoto.style.boxShadow = "unset";
+        content.style.color = "#f3f3f2"
+        quizText.style.backgroundColor = "#2A2A2A";
+        takeMeToQuiz.style.backgroundColor = "#a3b872";
+        newsletterPanel.style.backgroundColor = "#a3b872";
+    } else {
+        document.body.style.backgroundColor = "unset";
+        links.forEach(links => {
+            links.style.color = "unset"; 
+        });
+        content.style.color = "unset"
+        takeMeToQuiz.style.backgroundColor = "#5C7315";
+        newsletterPanel.style.backgroundColor = "#5C7315";
+    }
+
+    console.log(clickCounter);
+});
+
 //homepage button action to take user to quiz page
 const takeMeToQuizButton = document.querySelector("#takeMeToQuiz");
 
@@ -5,6 +41,7 @@ takeMeToQuizButton.addEventListener("click", () =>{
     location.href = 'html/quiz.html';
 });
 
+//when you click page logo you get taken to homepage
 const takeMeToHomepageButton = document.querySelector("#mucholapaLogoImg");
 
 takeMeToHomepageButton.addEventListener("click", () =>{
@@ -16,7 +53,6 @@ const emailInput = document.querySelector("#emailInput");
 const done = document.querySelector("#done");
 const newsletterText = document.querySelector("#newsletterText");
 const newsletterForm = document.querySelector("#newsletterForm");
-const newsletterPanel = document.querySelector("#newsletterPanel");
 
 //newsletter
 function  validateEmail(){
