84edde85d968d730a0bbc4bb499dabfc42301835
BlairStChair <julia.z15@interia.pl>
2024-10-07 18:23:02 +0200

    Started implementing option to bigger the font size

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const darkMode = document.querySelector("#darkMode");
+const biggerFont = document.querySelector("#biggerFont");
+
 const links = document.querySelectorAll("a");
+const accesibilityBtns = document.querySelectorAll(".accesibilityBtns");
+const accesibilityImg = document.querySelectorAll(".accesibilityImg");
+const mucholapaLogoImg = document.querySelector("#mucholapaLogoImg")
 const contentPhoto = document.querySelector("#contentPhoto");
 const content = document.querySelector("#content");
 const quizText = document.querySelector("#quizText");
+const quizText2 = document.querySelectorAll(".quizText");
+const quizTexth2 = document.querySelector("#quizTexth2");
+const quizTextp = document.querySelector("#quizTextp");
+const quizPreview = document.querySelector("#quizPreview");
 const takeMeToQuiz = document.querySelector("#takeMeToQuiz");
 const newsletterPanel = document.querySelector("#newsletterPanel");
 
-let clickCounter = 0;
+let clickCounterDM = 0;
+let clickCounterBF = 0;
 
 darkMode.addEventListener("click", () => {
-    clickCounter++;
+    clickCounterDM++;
 
-    if (clickCounter % 2 == 1) {
+    if (clickCounterDM % 2 == 1) {
         document.body.style.backgroundColor = "#1A1A1A";
         links.forEach(links => {
             links.style.color = "#f3f3f2"; 
@@ -30,8 +40,50 @@ darkMode.addEventListener("click", () => {
         takeMeToQuiz.style.backgroundColor = "#5C7315";
         newsletterPanel.style.backgroundColor = "#5C7315";
     }
+});
 
-    console.log(clickCounter);
+biggerFont.addEventListener("click", () => {
+    clickCounterBF++;
+
+    if (clickCounterBF % 2 == 1) {
+        accesibilityImg.forEach(accesibilityImg => {
+            accesibilityImg.style.width = "1.1rem"; 
+            accesibilityImg.style.height = "1.1rem"; 
+        });
+        accesibilityBtns.forEach(accesibilityBtns => {
+            accesibilityBtns.style.width = "2rem"; 
+            accesibilityBtns.style.height = "2rem"; 
+        });
+        mucholapaLogoImg.style.height = "20rem";
+        links.forEach(links => {
+            links.style.fontSize = "150%"; 
+        });
+        contentPhoto.style.height = "17rem";
+        contentPhoto.style.width = "17rem";
+        content.style.fontSize = "1.8rem";
+        quizText.style.width = "60rem";
+        quizTexth2.style.fontSize = "36px";
+        quizTextp.style.fontSize = "30px"
+    } else {
+        accesibilityImg.forEach(accesibilityImg => {
+            accesibilityImg.style.width = "0.8rem"; 
+            accesibilityImg.style.height = "0.8rem"; 
+        });
+        accesibilityBtns.forEach(accesibilityBtns => {
+            accesibilityBtns.style.width = "1.5rem"; 
+            accesibilityBtns.style.height = "1.5rem"; 
+        });
+        mucholapaLogoImg.style.height = "18rem";
+        links.forEach(links => {
+            links.style.fontSize = "unset"; 
+        });
+        contentPhoto.style.height = "15rem";
+        contentPhoto.style.width = "15rem";
+        content.style.fontSize = "1.5rem";
+        quizText.style.width = "56rem";
+        quizTexth2.style.fontSize = "28px";
+        quizTextp.style.fontSize = "22px"
+    }
 });
 
 //homepage button action to take user to quiz page
